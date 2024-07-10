8118344ad81087388c8e296bb6a4f6b23ad45f6f
lordeadroid <rishabhrawat34@gmail.com>
2024-07-10 18:04:22 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Tasks
+    </div>
+  );
+};
+
+export default App;
